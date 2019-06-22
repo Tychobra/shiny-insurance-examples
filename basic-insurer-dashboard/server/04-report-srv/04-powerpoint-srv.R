9d8e21d6116ee2e3ebc5bcc2950237181a5eb3dc
RichardHHill <richard_hill@brown.edu>
2019-06-22 17:15:46 -0400

    Added to powerpoint

--- basic-insurer-dashboard/server/04-report-srv/04-powerpoint-srv.R
+++ basic-insurer-dashboard/server/04-report-srv/04-powerpoint-srv.R
@@ -23,10 +23,43 @@ output$generate_ppt_report <- downloadHandler(
           paste0("Report Generated on ", format(Sys.Date(), "%B %d, %Y"))
         )
       ) %>% 
-      ph_with_img(
+      ph_with_img_at(
+        src = "server/04-report-srv/images/tychobra_logo_blue_co_name.png",
+        height = 1.5,
+        width = 4,
+        left = 3,
+        top = 0
+      ) %>% 
+      add_slide(layout = "Blank", master = "Office Theme") %>% 
+      ph_with_text(
         type = "ftr",
-        src = "server/04-report-srv/images/tychobra_logo_blue_co_name.png"
+        str = c(
+          "Exhibit 1",
+          "Summary of Loss & ALAE",
+          paste0("Evaluated as of ", format(input$val_date, "%B %d, %Y"))
+          )
+      ) %>% 
+      ph_with(
+        value = data.frame(x = 1:2),
+        location = ph_location_fullsize()
+      ) %>%
+      add_slide(layout = "Title and Content", master = "Office Theme") %>% 
+      ph_with_table_at(
+        value = data.frame(y = 5:10),
+        left = 2,
+        top = 0.5,
+        width = 4,
+        height = 5
+      ) %>% 
+      ph_with_text(
+        type = "dt",
+        str = c(
+          "Exhibit 2",
+          "Claims with charge in paid >= 100,000",
+          paste0("Evaluated as of ", format(input$val_date, "%B %d, %Y"))
+        )
       )
+      
     
     print(example_ppt, target = file)
   }
